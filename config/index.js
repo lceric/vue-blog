0664af2374d09586bf4823c704bde4612e03aca5
Eric <lichao@kaike.la>
2020-03-26 16:53:18 +0800

    build: 加入开发环境代理

--- config/index.js
+++ config/index.js
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'https://strun.club',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
